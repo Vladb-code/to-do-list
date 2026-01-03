9a0551e4d32e8a739ff3cd797e62a3d13e4a7785
Your Name <vlaslv.bycgkov.97@list.ru>
2026-01-03 18:14:56 +0300

    fix store

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,6 @@
 import { legacy_createStore, combineReducers } from "redux";
 import inputTextReduser from "./reducers/inputTextReducer";
-import tasksReduser from "./reducers/taskReducer";
+import tasksReduser from "./reducers/tasksReduser";
 
 const store = legacy_createStore(
   combineReducers({
